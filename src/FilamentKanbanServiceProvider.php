c8b17a92d5eb214ebbcf225c3067d6786241044c
mokhosh <mokhosh@users.noreply.github.com>
2023-11-17 12:05:31 +0000

    Fix styling

--- src/FilamentKanbanServiceProvider.php
+++ src/FilamentKanbanServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Mokhosh\FilamentKanban\Commands\FilamentKanbanCommand;
+use Mokhosh\FilamentKanban\Testing\TestsFilamentKanban;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Mokhosh\FilamentKanban\Commands\FilamentKanbanCommand;
-use Mokhosh\FilamentKanban\Testing\TestsFilamentKanban;
 
 class FilamentKanbanServiceProvider extends PackageServiceProvider
 {
