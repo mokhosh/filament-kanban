310ab993b9af80319150e108765b8369b4d974b4
Mo Khosh <mskhoshnazar@gmail.com>
2023-11-27 18:17:45 +0330

    add basic styles

--- resources/views/pages/kanban-board.blade.php
+++ resources/views/pages/kanban-board.blade.php
@@ -1,12 +1,12 @@
 <x-filament-panels::page>
-    <div class="flex">
+    <div class="md:flex overflow-x-auto gap-6">
         @foreach($statuses as $status)
-            <div class="w-56">
-                <h3>{{ $status['title'] }}</h3>
+            <div class="md:w-[24rem] flex-shrink-0">
+                <h3 class="font-semibold text-lg text-gray-400 mb-4">{{ $status['title'] }}</h3>
 
-                <div>
+                <div class="flex flex-col gap-2">
                     @foreach($status['records'] as $record)
-                        <div>{{ $record['title'] }}</div>
+                        <div class="bg-white rounded border px-4 py-2 cursor-grab font-medium text-gray-600">{{ $record['title'] }}</div>
                     @endforeach
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
+        './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
 }
