35c0de7572f1b4cc7d8c31d959e20cb8c06c2209
Mo Kawsara <mkwsra@gmail.com>
2024-01-07 21:24:23 +0400

    change status header style

--- resources/views/kanban-header.blade.php
+++ resources/views/kanban-header.blade.php
@@ -1,1 +1,4 @@
-<h3 class="px-4 font-semibold text-lg text-gray-400">{{ $status['title'] }}</h3>
+<div
+    class="m-2 p-4 bg-white border-solid border-0 border-t rounded-lg dark:bg-gray-800 dark:border-gray-700">
+    <h3 class="text-lg font-semibold text-gray-400">{{ $status['title'] }}</h3>
+</div>
