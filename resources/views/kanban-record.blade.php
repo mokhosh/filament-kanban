fdde849fdba5d2225585f239d6b6f677f4ed230c
Mo Kawsara <mkwsra@gmail.com>
2024-01-07 21:24:55 +0400

    customize main record attributes and minor style added

--- resources/views/kanban-record.blade.php
+++ resources/views/kanban-record.blade.php
@@ -1,7 +1,28 @@
+@php use Illuminate\Contracts\Support\Htmlable; @endphp
 <div
     id="{{ $record['id'] }}"
     wire:click="recordClicked('{{ $record['id'] }}', {{ @json_encode($record) }})"
-    class="record bg-white rounded border px-4 py-2 cursor-grab font-medium text-gray-600"
+    class="record cursor-grab block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-50 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 >
-    {{ $record['title'] }}
+
+    @if($record['title'] instanceof Htmlable)
+        {{$record['title']}}
+    @elseif($record['title'])
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary-500 dark:text-primary-500">
+            {{$record['title']}}
+        </h5>
+    @else
+        {{-- Fallback to record ID if title is not set. --}}
+        <h5>
+            {{$record['id']}}
+        </h5>
+    @endif
+
+    @if($record['content'] instanceof Htmlable)
+        {{$record['content']}}
+    @elseif($record['content'])
+        <p class="font-normal text-gray-700 dark:text-gray-400">{{ $record['content'] }}</p>
+    @endif
+
+
 </div>
--- src/Pages/KanbanBoard.php
+++ src/Pages/KanbanBoard.php
@@ -4,6 +4,7 @@ namespace Mokhosh\FilamentKanban\Pages;
 
 use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Mokhosh\FilamentKanban\Concerns\HasEditRecordModal;
@@ -19,9 +20,69 @@ class KanbanBoard extends Page implements HasForms
 
     protected static string $view = 'filament-kanban::kanban-board';
 
-    protected static string $recordTitleAttribute = 'title';
+    /**
+     * The cardTitle property defines the database column or HTML content
+     * that will be used to display the title for each record in the Kanban board.
+     *
+     * By default, this is set to 'title' which will display the title attribute
+     * of each record. However, you can override this and return any HTMLable
+     * object to fully customize the content and styling for each card.
+     *
+     * For example:
+     *
+     * ```php
+     * public function getCardTitle(?Model $record) {
+     *   return new HtmlString('<span>'.$record->uuid.'</span><h3>'.$record->title.'</h3>');
+     * }
+     * ```
+     *
+     * This would allow you to output custom HTML content for each record's card.
+     *
+     * @var string|Htmlable|callable
+     */
+    protected static $cardTitle = 'title';
+
+    /**
+     * The cardContent property defines the database column or HTML content
+     * that will be used to display the card content for each record in the Kanban board.
+     *
+     * By default, this is set to 'null' which will not display anything
+     * of each record. However, you can override this and return any HTMLable
+     * object to fully customize the content and styling for each card.
+     *
+     * For example:
+     *
+     * ```php
+     * public function getCardContent(?Model $record) {
+     *   return new HtmlString('<b class="text-gray-300">'.$record->created_at->diffForHumans().'</b><br><p>'.$record->description.'</p>');
+     * }
+     * ```
+     *
+     * This would allow you to output custom HTML content for each record's card.
+     *
+     * @var null|string|Htmlable|callable
+     */
+    protected static $cardContent;
 
     protected static string $recordStatusAttribute = 'status';
+
+    /**
+     * If true, a divider will be shown under the card title.
+     *
+     * This can be used to visually separate the title from the rest of the
+     * card content.
+     */
+    protected static bool $enableDividerAfterTitle = false;
+
+    public function getCardTitle(?Model $record): string | Htmlable | null
+    {
+        return static::$cardTitle;
+    }
+
+    public function getCardContent(?Model $record): string | Htmlable | null
+    {
+        return static::$cardContent;
+    }
 
     protected function statuses(): Collection
     {
@@ -51,17 +112,44 @@ class KanbanBoard extends Page implements HasForms
 
     protected function transformRecords(Model $record): Collection
     {
-        return collect([
+        return collect(value: [
             'id' => $record->id,
-            'title' => $record->{static::$recordTitleAttribute},
-            'status' => $record->{static::$recordStatusAttribute} instanceof UnitEnum ?
-                $record->{static::$recordStatusAttribute}->value :
-                $record->{static::$recordStatusAttribute},
+            'title' => $this->determineCardTitle($record),
+            'content' => $this->determineCardContent($record),
+            'status' => $this->determineStatus($record),
         ])->merge($this->additionalRecordData($record));
     }
 
     protected function additionalRecordData(Model $record): Collection
     {
         return collect([]);
+    }
+
+    private function determineCardTitle(Model $record): mixed
+    {
+        $cardTitle = $this->getCardTitle($record);
+
+        if (is_string($cardTitle) && array_key_exists($cardTitle, $record->getAttributes())) {
+            return $record->{$cardTitle};
+        }
+
+        return $cardTitle;
+    }
+
+    private function determineCardContent(Model $record): mixed
+    {
+        $cardContent = $this->getCardContent($record);
+        if (is_string($cardContent) && array_key_exists($cardContent, $record->getAttributes())) {
+            return $record->{$cardContent};
+        }
+
+        return $cardContent;
+    }
+
+    private function determineStatus(Model $record): mixed
+    {
+        $recordStatus = $record->{static::$recordStatusAttribute};
+
+        return ($recordStatus instanceof UnitEnum) ? $recordStatus->value : $recordStatus;
     }
 }
