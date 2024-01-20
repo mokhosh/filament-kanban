c73fbbbdba52996691ce9da1a723b92445f54407
Mo Khosh <mskhoshnazar@gmail.com>
2024-01-21 01:16:29 +0330

    modernize and add dark mode

--- resources/views/kanban-record.blade.php
+++ resources/views/kanban-record.blade.php
@@ -1,7 +1,7 @@
 <div
     id="{{ $record['id'] }}"
     wire:click="recordClicked('{{ $record['id'] }}', {{ @json_encode($record) }})"
-    class="record bg-white rounded border px-4 py-2 cursor-grab font-medium text-gray-600"
+    class="record transition bg-white dark:bg-gray-700 rounded-lg px-4 py-2 cursor-grab font-medium text-gray-600 dark:text-gray-200"
 >
     {{ $record['title'] }}
 </div>
