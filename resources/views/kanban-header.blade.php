596febd841f48c33b03676c4445fb01fdc1c62c2
Mo Khosh <mskhoshnazar@gmail.com>
2024-01-21 01:11:33 +0330

    bottom padding + embelishment

--- resources/views/kanban-header.blade.php
+++ resources/views/kanban-header.blade.php
@@ -1,1 +1,4 @@
-<h3 class="px-4 font-semibold text-lg text-gray-400">{{ $status['title'] }}</h3>
+<h3 class="mb-2 px-4 font-semibold text-lg text-gray-400">
+    <span class="text-primary-400">❖</span>
+    {{ $status['title'] }}
+</h3>
