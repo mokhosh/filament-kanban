276a1cbd938ab1a64d069e22285ef0ccca8b4890
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-08 01:46:54 +0330

    remove unnecessary overrides

--- tests/Pages/TestBoardWithCustomViews.php
+++ tests/Pages/TestBoardWithCustomViews.php
@@ -19,37 +19,10 @@ class TestBoardWithCustomViews extends KanbanBoard
         return TaskStatus::statuses();
     }
 
-    protected function records(): Collection
-    {
-        return Task::latest('updated_at')->get();
-    }
-
-    public function onStatusChanged(int $recordId, string $status, array $fromOrderedIds, array $toOrderedIds): void
-    {
-        Task::find($recordId)->update(['status' => $status]);
-    }
-
-    public function onSortChanged(int $recordId, string $status, array $orderedIds): void
-    {
-        Task::setNewOrder($orderedIds);
-    }
-
     protected function getEditModalFormSchema(?int $recordId): array
     {
         return [
             TextInput::make('title'),
         ];
-    }
-
-    protected function getEditModalRecordData($recordId, $data): array
-    {
-        return Task::find($recordId)->toArray();
-    }
-
-    protected function editRecord($recordId, array $data, array $state): void
-    {
-        Task::find($recordId)->update([
-            'title' => $data['title'],
-        ]);
     }
 }
