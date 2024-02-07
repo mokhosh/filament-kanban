ec974672d6afee0f116745db5d2595414b0debc8
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-08 02:38:27 +0330

    use the record directly

--- resources/views/kanban-record.blade.php
+++ resources/views/kanban-record.blade.php
@@ -1,8 +1,8 @@
 <div
-    id="{{ $record['id'] }}"
-    wire:click="recordClicked('{{ $record['id'] }}', {{ @json_encode($record) }})"
+    id="{{ $record->getKey() }}"
+    wire:click="recordClicked('{{ $record->getKey() }}', {{ @json_encode($record) }})"
     class="record transition bg-white dark:bg-gray-700 rounded-lg px-4 py-2 cursor-grab font-medium text-gray-600 dark:text-gray-200"
-    @if($record['just_updated'])
+    @if($record->just_updated)
         x-data
         x-init="
             $el.classList.add('animate-pulse-twice', 'bg-primary-100', 'dark:bg-primary-800')
@@ -14,5 +14,5 @@
         "
     @endif
 >
-    {{ $record['title'] }}
+    {{ $record->{static::$recordTitleAttribute} }}
 </div>
