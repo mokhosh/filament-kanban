32dfdf00750719d16b732bdd7137522f52527d6c
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:33:07 +0330

    add sortable interface and trait

--- tests/Models/Task.php
+++ tests/Models/Task.php
@@ -3,8 +3,12 @@
 namespace Mokhosh\FilamentKanban\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class Task extends Model
+class Task extends Model implements Sortable
 {
+    use SortableTrait;
+
     protected $guarded = [];
 }
