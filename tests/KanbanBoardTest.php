5c2f69599527cbce1d2a242804faaeee95f6233d
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:33:50 +0330

    test changes sort

--- tests/KanbanBoardTest.php
+++ tests/KanbanBoardTest.php
@@ -38,3 +38,23 @@ it('changes status', function () {
 
     expect($task->fresh()->status)->toBe(TaskStatus::Done->value);
 });
+
+it('changes sort', function () {
+    $task1 = Task::create([
+        'title' => 'First Task',
+        'status' => 'Todo',
+    ]);
+    $task2 = Task::create([
+        'title' => 'Second Task',
+        'status' => 'Todo',
+    ]);
+
+    expect($task1->order_column)
+        ->toBe(1);
+
+    livewire(TestBoard::class)
+        ->call('onSortChanged', $task1->id, TaskStatus::Todo->value, [$task2->id, $task1->id]);
+
+    expect($task1->fresh()->order_column)
+        ->toBe(2);
+});
