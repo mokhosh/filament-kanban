081b0c466a1bc62919b80c71891541cc814213fb
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:22:48 +0330

    test changes status

--- tests/KanbanBoardTest.php
+++ tests/KanbanBoardTest.php
@@ -4,6 +4,7 @@ use Mokhosh\FilamentKanban\Tests\Enums\TaskStatus;
 use Mokhosh\FilamentKanban\Tests\Models\Task;
 use Mokhosh\FilamentKanban\Tests\Pages\TestBoard;
 use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
 
 it('loads statuses', function () {
     $statuses = TaskStatus::statuses()
@@ -24,4 +25,16 @@ it('loads records', function () {
     actingAs($this->admin)
         ->get(TestBoard::getUrl())
         ->assertSee($task->title);
+});
+
+it('changes status', function () {
+    $task = Task::create([
+        'title' => 'First Task',
+        'status' => 'Todo',
+    ]);
+
+    livewire(TestBoard::class)
+        ->call('onStatusChanged', $task->id, TaskStatus::Done->value, [], []);
+
+    expect($task->fresh()->status)->toBe(TaskStatus::Done->value);
 });
