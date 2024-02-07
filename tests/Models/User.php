d4d32afa5dc95a1d9cf1a453b771a8c0f25b2984
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:06:19 +0330

    add test user model

---
+++ tests/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mokhosh\FilamentKanban\Tests\Models;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements FilamentUser
+{
+    protected $guarded = [];
+    public $timestamps = false;
+    protected $table = 'users';
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+}
