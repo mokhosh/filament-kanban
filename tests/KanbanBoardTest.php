fbeac4e619a2a6328f92a62563eddd5175579ebb
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:13:08 +0330

    test loads statuses

--- tests/KanbanBoardTest.php
+++ tests/KanbanBoardTest.php
@@ -1,8 +1,19 @@
 <?php
 
+use Mokhosh\FilamentKanban\Tests\Enums\TaskStatus;
 use Mokhosh\FilamentKanban\Tests\Models\Task;
 use Mokhosh\FilamentKanban\Tests\Pages\TestBoard;
 use function Pest\Laravel\actingAs;
+
+it('loads statuses', function () {
+    $statuses = TaskStatus::statuses()
+        ->pluck('title')
+        ->toArray();
+
+    actingAs($this->admin)
+        ->get(TestBoard::getUrl())
+        ->assertSeeInOrder($statuses);
+});
 
 it('loads records', function () {
     $task = Task::create([
