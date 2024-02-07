a506450f9f0a143d377b8c7b4510854bf2e8c0cb
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-07 23:41:25 +0330

    use factory + cast status to enum

--- tests/Models/Task.php
+++ tests/Models/Task.php
@@ -2,13 +2,20 @@
 
 namespace Mokhosh\FilamentKanban\Tests\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mokhosh\FilamentKanban\Tests\Enums\TaskStatus;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 class Task extends Model implements Sortable
 {
+    use HasFactory;
     use SortableTrait;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
 }
