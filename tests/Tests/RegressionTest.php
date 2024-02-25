cbbbcf9463ee85fd6109a68422bcd60303b8ab82
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-25 13:50:26 +0330

    move regression tests to a separate file

---
+++ tests/Tests/RegressionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Mokhosh\FilamentKanban\Tests\Models\Task;
+use Mokhosh\FilamentKanban\Tests\Pages\TestBoard;
+use function Pest\Laravel\actingAs;
+
+it('doesnt break if model should be strict', function () {
+    Model::shouldBeStrict();
+
+    $task = Task::factory()->create();
+
+    actingAs($this->admin)
+        ->get(TestBoard::getUrl())
+        ->assertSee($task->title);
+
+    Model::shouldBeStrict(false);
+});
