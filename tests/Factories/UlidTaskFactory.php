b8dd3689a9dfee2d26b8b18a27f87fad2bf74573
Mo Khosh <mskhoshnazar@gmail.com>
2024-03-18 23:09:18 +0330

    add ulid task factory

---
+++ tests/Factories/UlidTaskFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mokhosh\FilamentKanban\Tests\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Mokhosh\FilamentKanban\Tests\Enums\TaskStatus;
+use Mokhosh\FilamentKanban\Tests\Models\UlidTask;
+
+class UlidTaskFactory extends Factory
+{
+    protected $model = UlidTask::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'status' => fake()->randomElement(TaskStatus::cases()),
+        ];
+    }
+
+    public function todo()
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::Todo,
+        ]);
+    }
+
+    public function doing()
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::Doing,
+        ]);
+    }
+
+    public function done()
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::Done,
+        ]);
+    }
+}
