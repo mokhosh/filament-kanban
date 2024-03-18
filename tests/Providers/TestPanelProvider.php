e581cc13757f7ad3af84d596f6b8bf044f76a111
Mo Khosh <mskhoshnazar@gmail.com>
2024-03-18 23:10:04 +0330

    add ulid task board to test panel

--- tests/Providers/TestPanelProvider.php
+++ tests/Providers/TestPanelProvider.php
@@ -16,6 +16,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Mokhosh\FilamentKanban\Tests\Pages\TestBoard;
 use Mokhosh\FilamentKanban\Tests\Pages\TestBoardWithCustomViews;
+use Mokhosh\FilamentKanban\Tests\Pages\TestBoardWithUlidTask;
 
 class TestPanelProvider extends PanelProvider
 {
@@ -27,6 +28,7 @@ class TestPanelProvider extends PanelProvider
             ->pages([
                 TestBoard::class,
                 TestBoardWithCustomViews::class,
+                TestBoardWithUlidTask::class,
             ])
             ->resources([
 
