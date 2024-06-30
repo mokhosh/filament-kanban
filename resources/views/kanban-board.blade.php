de7131087b276a02d1cde5d29a9cd3352aae183a
Milen Karaganski <milenmk@gmail.com>
2024-06-30 17:51:43 +0300

    Layout and background change Added script to show/hide columns

--- resources/views/kanban-board.blade.php
+++ resources/views/kanban-board.blade.php
@@ -1,4 +1,6 @@
 <x-filament-panels::page>
+    <div id="hiddenItems" class="flex gap-5 bg-white rounded-lg p-2">{{ __('Hidden columns') }}:</div>
+
     <div x-data wire:ignore.self class="md:flex overflow-x-auto overflow-y-hidden gap-4 pb-4">
         @foreach($statuses as $status)
             @include(static::$statusView)
