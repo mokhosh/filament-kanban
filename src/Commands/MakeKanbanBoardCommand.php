06a09aa96f1a9cb19aecdba193048026d039f581
YNWA Fawzy <38886749+Cybrarist@users.noreply.github.com>
2024-10-01 17:21:19 +0400

    add force flag to force to recreate the board if it already exists

--- src/Commands/MakeKanbanBoardCommand.php
+++ src/Commands/MakeKanbanBoardCommand.php
@@ -6,11 +6,12 @@ use Illuminate\Console\GeneratorCommand;
 
 class MakeKanbanBoardCommand extends GeneratorCommand
 {
-    public $name = 'make:kanban';
-
     public $description = 'Create a filament kanban board page';
 
     public $type = 'Kanban page';
+
+    protected $signature = 'make:kanban {name}
+    {--force : Force kanban board to recreated}';
 
     protected function getStub()
     {
