4f472ecce3c58df05a105ae1b42999365102d550
chandraauliatama <chandraaulia4@gmail.com>
2025-06-21 20:07:49 +0700

    Fix status badge color not working

--- resources/views/kanban-header.blade.php
+++ resources/views/kanban-header.blade.php
@@ -4,10 +4,18 @@
         <div class="flex items-center">
             <span class="block text-sm font-semibold mr-2" x-text="status.title"></span>
 
-
-            <x-filament::badge size="sm" x-bind:color="status.badgeColor">
+            <span class="inline-flex items-center justify-center gap-x-1 rounded-md text-xs font-medium ring-1 ring-inset px-1.5 min-w-[theme(spacing.5)] py-0.5 tracking-tight"
+                x-bind:class="{
+                    'bg-gray-50 text-gray-600 ring-gray-600/10 dark:bg-gray-400/10 dark:text-gray-400 dark:ring-gray-400/20': status.badgeColor === 'gray',
+                    'fi-color-custom bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30': status.badgeColor !== 'gray'
+                }"
+                x-bind:style="status.badgeColor !== 'gray' ? {
+                    '--c-50': 'var(--' + status.badgeColor + '-50)',
+                    '--c-400': 'var(--' + status.badgeColor + '-400)', 
+                    '--c-600': 'var(--' + status.badgeColor + '-600)'
+                } : {}">
                 <span x-text="status.records.length"></span>
-            </x-filament::badge>
+            </span>
         </div>
 
         <template x-if="status.totalAttribute">
